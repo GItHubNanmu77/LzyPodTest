09dadc3cc03c2796008be92738279943cec68d51
lzy <>
2019-06-17 17:17:13 +0800

    commit project

---
+++ postest/podtest/podtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  podtest
+//
+//  Created by cisdi on 2019/6/17.
+//  Copyright © 2019 lzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ postest/podtest/podtest/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  podtest
+//
+//  Created by cisdi on 2019/6/17.
+//  Copyright © 2019 lzy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
